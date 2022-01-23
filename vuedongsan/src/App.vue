50e5664787faf9e460f6e827cc1d5bb4f4d6073d
yunghsin Lee <lenalee7963@gmail.com>
2022-01-23 22:26:31 +0900

    new commit2

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,16 +1,25 @@
 <template>
 
+  <div class="black-bg" v-if="modal==true">
+  <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지의 내용</p>
+      <button @click="modal=false" class="modal-btn">닫기</button>
+  </div>
+  </div>
 
   <div class="menu">
     <a v-for="(a,i) in menu" :key="i">{{a}}</a>
   </div>
 
   <div v-for="(a,i) in products" :key="i">
-    <h4>{{products[i]}}</h4>
-    <p>{{price[i]}}</p>
+    <img src='./assets/room0.jpg' class="room-img">
+    <h4 @click="modal=true">{{products[i]}}</h4>
+    <p>{{price[i]}}만원</p>
     <button @click="increase(i)">허위매물신고</button> <span>신고수 : {{신고수[i]}}</span>
   </div>
   
+
 
 </template>
 
@@ -20,16 +29,23 @@ export default {
   name: 'App',
   data(){
     return{
+      modal:false,
       menu:['Home','Products','About'],
       price: [10,20,30],
       products:['역삼동원룸','천호동원룸','중랑구빌라'],
-      신고수:[0,0,0]
+      신고수:[0,0,0],
+      link:['room0.jpg','room1.jpg','room2.jpg']
       
     }//뷰에서의 데이터 보관함 
   },
   methods:{
     increase(i){
       this.신고수[i]++;// vue에서는 this로 
+    },
+    url(i){
+
+      
+      return this.link[i];
     }
 
 
@@ -38,9 +54,35 @@ export default {
     
   }
 }
+
+
 </script>
 
+
+
 <style>
+body{
+  margin:0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width:100%;height:100%;
+  background: rgba(0,0,0,0,5);
+  position: fixed;
+  padding:20px;
+}
+.white-bg{
+  width:100%;
+  background: rgb(221, 202, 202);
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img{
+  width:40%;
+  margin-top:40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,5 +98,12 @@ export default {
 .menu a{
   color: white;
   padding: 10px;
+}
+.modal-btn{
+  color: white;
+  background: red;
+  border:1px solid red;
+  border-radius: 5px;
+  
 }
 </style>
